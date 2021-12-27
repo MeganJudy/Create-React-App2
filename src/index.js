465eee529fc1854c87d2f50222a4e3d29c6f0f79
Megan Judy <meganrosejudy1@gmail.com>
2021-12-26 20:08:46 -0500

    Header & Footer

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
